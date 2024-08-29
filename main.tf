7194cbb5be748dfb3dbfee75caa486a584d814db
Ebrahim Espootin <ebrahimespootin@Ebrahim--MacBook-Pro.local>
2024-08-29 19:05:52 +0200

    a7

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "azurerm" {
 
   }
   subscription_id = "8fcdb956-7c93-4e3c-8bac-8d4b21641d81"
+  tenant_id = "66acafd5-dde2-4514-b99d-106450ba1984"
 }
 
 resource "azurerm_resource_group" "Pipelines-Dev-CIndia-RG" {
