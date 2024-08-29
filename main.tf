d3c7f595899c3511789da66a1378b6405b854499
Ebrahim Espootin <ebrahimespootin@Ebrahim--MacBook-Pro.local>
2024-08-29 19:26:11 +0200

    a8

--- main.tf
+++ main.tf
@@ -1,21 +1,11 @@
 provider "azurerm" {
-  features {
-
-  }
+  features {}
   subscription_id = "8fcdb956-7c93-4e3c-8bac-8d4b21641d81"
-  tenant_id = "66acafd5-dde2-4514-b99d-106450ba1984"
+  #tenant_id = "66acafd5-dde2-4514-b99d-106450ba1984"
 }
 
-resource "azurerm_resource_group" "Pipelines-Dev-CIndia-RG" {
+resource "azurerm_resource_group" "tfexample" {
   name     = "Pipelines-Dev-CIndia-RG"
   location = "Central India"
-}
-
-resource "azurerm_storage_account" "etldevindiastg2" {
-  name                     = "etldevindiastg2"
-  resource_group_name      = "Pipelines-Dev-CIndia-RG"
-  location                 = "centralindia"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
 }
 
