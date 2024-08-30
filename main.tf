a8f51e93bfb0b368fff6f483a58e650a6a59abfa
Ebrahim Espootin <e.espootin@gmail.com>
2024-08-30 12:57:35 +0200

    a15

--- main.tf
+++ main.tf
@@ -4,8 +4,16 @@ provider "azurerm" {
   #tenant_id = "66acafd5-dde2-4514-b99d-106450ba1984"
 }
 
-resource "azurerm_resource_group" "tfexample" {
+resource "azurerm_resource_group" "Pipelines-Dev-CIndia-RG" {
   name     = "Pipelines-Dev-CIndia-RG"
   location = "Central India"
+}
+
+resource "azurerm_storage_account" "etldevindiastg2" {
+  name                     = "etldevindiastg2"
+  resource_group_name      = "Pipelines-Dev-CIndia-RG"
+  location                 = "centralindia"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
